92a474fdfba27c0dcbdaf3d5877477fb4c8cc98f
Bujar Bakiu <bujar@data-max.io>
2025-12-08 21:00:00 +0100

    added deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://data-max-hq.github.io',
+  base: '/galaxy-website/',
   vite: {
     plugins: [tailwindcss()]
   }
